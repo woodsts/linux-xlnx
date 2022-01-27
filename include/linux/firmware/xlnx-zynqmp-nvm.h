0254a25939e866130faf4aab1d5c0d50dacd9e51
Harsha <harsha.harsha@xilinx.com>
2022-01-27 15:23:25 +0100

    drivers: firmware: xilinx: Add support to access BBRAM and volatile user keys
    This patch adds below support in the firmware:
     - Write BBRAM AES Key
     - Zeroize AES Key
     - Write User Data (32-bits BBRAM user space)
     - Read User Data (32-bits BBRAM user space)
     - Lock User Data for write (32-bits BBRAM user space)
     - Write volatile user keys
    
    Signed-off-by: Harsha <harsha.harsha@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending
    [michals: v6.6
    Align with new firmware function call
    v6.12 update
    firmware: xilinx: Remove zynqmp_pm_write_aes_key*()
    v6.18 update
    Subject update
    Rework around split
    ]

--- include/linux/firmware/xlnx-zynqmp-nvm.h
+++ include/linux/firmware/xlnx-zynqmp-nvm.h
@@ -9,13 +9,48 @@
 #define __FIRMWARE_ZYNQMP_NVM_H__
 
 /* NVM Commands */
+#define PM_BBRAM_WRITE_KEY		0xB01
+#define PM_BBRAM_ZEROIZE		0xB02
+#define PM_BBRAM_WRITE_USERDATA		0xB03
+#define PM_BBRAM_READ_USERDATA		0xB04
+#define PM_BBRAM_LOCK_USERDATA		0xB05
 #define PM_EFUSE_READ_VERSAL		0xB17
 
 #if IS_REACHABLE(CONFIG_ZYNQMP_FIRMWARE)
+int zynqmp_pm_bbram_write_usrdata(u32 data);
+int zynqmp_pm_bbram_read_usrdata(const u64 outaddr);
+int zynqmp_pm_bbram_write_aeskey(u32 keylen, const u64 keyaddr);
+int zynqmp_pm_bbram_zeroize(void);
+int zynqmp_pm_bbram_lock_userdata(void);
 int zynqmp_pm_efuse_access(const u64 address, u32 *out);
 int versal_pm_efuse_read(const u64 address, u32 offset, u32 size);
 int versal_pm_efuse_write(const u64 address, const u32 operationid, const u8 envdis);
 #else
+static inline int zynqmp_pm_bbram_write_usrdata(u32 data)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_bbram_read_usrdata(const u64 outaddr)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_bbram_write_aeskey(const u64 keyaddr, u16 keylen)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_bbram_zeroize(void)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_bbram_lock_userdata(void)
+{
+	return -ENODEV;
+}
+
 static inline int versal_pm_efuse_read(const u64 address, u32 offset, u32 size)
 {
 	return -ENODEV;
