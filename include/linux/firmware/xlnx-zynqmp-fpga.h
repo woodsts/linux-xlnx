d99606eeb44bbfc58455d6504da6ce395d5817ad
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2023-12-08 12:14:27 +0530

    drivers: firmware: xilinx: Add pm api function for PL readback
    Add PM API for performing PL configuration data readback.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: not-upstreamable
    Reason: The existing implementation will not cover all the use cases
    and also it uses sysfs interface to read back the PL contents.
    It may lead to security constraints.
    [michals:
    v6.18 update
    Subject update
    Rework around split
    ]

--- include/linux/firmware/xlnx-zynqmp-fpga.h
+++ include/linux/firmware/xlnx-zynqmp-fpga.h
@@ -21,10 +21,19 @@
 #define XILINX_ZYNQMP_PM_FPGA_READ_CONFIG_REG		0U
 
 #if IS_REACHABLE(CONFIG_ZYNQMP_FIRMWARE)
+int zynqmp_pm_fpga_read(const u32 reg_numframes, const u64 phys_address,
+			u32 readback_type, u32 *value);
 int zynqmp_pm_fpga_load(const u64 address, const u32 size, const u32 flags);
 int zynqmp_pm_fpga_get_status(u32 *value);
 int zynqmp_pm_fpga_get_config_status(u32 *value);
 #else
+static inline int zynqmp_pm_fpga_read(const u32 reg_numframes,
+				      const u64 phys_address, u32 readback_type,
+				      u32 *value)
+{
+	return -ENODEV;
+}
+
 static inline int zynqmp_pm_fpga_load(const u64 address, const u32 size,
 				      const u32 flags)
 {
