d6d10c120a4998ce4ba413545cdd3339475fc8ab
Manish Narani <manish.narani@xilinx.com>
2024-11-18 16:00:51 +0100

    usb: dwc3: Add power management support in DWC3 Xilinx driver
    Add support for power management features in DWC3 Xilinx driver. This
    includes registering PMU regulator and support to transition from/to
    D0/D3 states. Also separate out suspend and resume APIs to include D0/D3
    transitions.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    Signed-off-by: Piyush Mehta <piyush.mehta@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    [radheys squashed with:
    usb: dwc3: add support for enabling/disabling hibernation dynamically
    usb: dwc3: gadget: Add hibernation support when operating in gadget mode
    usb: dwc3: core: Add regulator calls for communicating with dwc3 PMU
    usb: dwc3: hibernation: fix scratch buffer allocation
    usb: dwc3: Fix the logic for allocating and setting scratchpad buffers
    usb: dwc3: use proper typecast to handle overflow
    usb: dwc3: handle usb2.0 gadget mode early hibernation event occurrence
    usb: dwc3: Fixed common regulator for multiple controller instance
    usb: dwc3: core: add PM_SLEEP kernel config check
    usb: dwc3: gadget: disable hibernation interrupt on gadget enumeration
    usb: dwc3: xilinx: Add support for controller power state for host mode
    usb: dwc3: xilinx: Handle dr_mode setting from DT properly
    usb: xhci-plat: Add remote wakeup support
    usb: dwc3: fix coverity warning CHECKED_RETURN
    usb: dwc3: Added remote wake-up in xilinx glue driver
    usb: dwc3: gadget: fix IRQ hiber not found error message
    usb: dwc3: core: regulator enabled on resume
    usb: dwc3: retain resource index for unforced stops
    v6.12 update
    usb: dwc3: Export symbol dwc3_host_wakeup_register/capable
    usb: dwc3: xilinx: add power-domains property support
    usb: dwc3: xilinx: enable pme interrupt for versal
    usb: dwc3: dwc3-xilinx: added usb-wakeup irq support
    usb: dwc3: core: Add support for disabling vbus when suspended
    usb: dwc3: gadget: switch to 'wakeup' interrupt name for hibernation support
    usb: dwc3: define dummy dwc3_host_wakeup_* APIs for DWC3_GADGET mode
    usb: dwc3: define dummy dwc3_gadget_*_hibernation APIs for DWC3_HOST mode
    usb: dwc3: xilinx: Cleanup ARCH_VERSAL kconfig dependency
    usb: dwc3: Fix kernel-doc description in gadget_hibernation.c
    usb: host: describe dependency on dwc3 host driver
    dwc3: core: call ulpi_write APIs only if ULPI PHY interface is enabled
    Resolve conflicts with de7ecc4e0570 ("usb: dwc3: xilinx: add reset-controller support")
    Move xhci.h from core.h to host.c and remove xhci-ext-caps.h from host.c
    v6.18 update
    usb: dwc3: core: Remove unused mask_phy_rst flag
    usb: dwc3: core: Fix kernel-doc comment style
    usb: dwc3: xilinx: Fix crash in remove path for gadget mode
    ]
    State: pending

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -28,6 +28,7 @@
 #include <linux/usb/otg.h>
 #include <linux/usb/role.h>
 #include <linux/ulpi/interface.h>
+#include <linux/ulpi/driver.h>
 
 #include <linux/phy/phy.h>
 
@@ -508,6 +509,7 @@
 
 /* Device Status Register */
 #define DWC3_DSTS_DCNRD			BIT(29)
+#define DWC3_DSTS_SRE			BIT(28)
 
 /* This applies for core versions 1.87a and earlier */
 #define DWC3_DSTS_PWRUPREQ		BIT(24)
@@ -675,6 +677,11 @@
 
 /* Force Gen1 speed on Gen2 link */
 #define DWC3_LLUCTL_FORCE_GEN1		BIT(10)
+
+/* ULPI control registers */
+#define ULPI_OTG_CTRL_SET		0xB
+#define ULPI_OTG_CTRL_CLEAR		0XC
+#define OTG_CTRL_DRVVBUS_OFFSET		BIT(5)
 
 /* Structures */
 
@@ -1008,10 +1015,12 @@ struct dwc3_glue_ops {
  * @drd_work: workqueue used for role swapping
  * @ep0_trb: trb which is used for the ctrl_req
  * @bounce: address of bounce buffer
+ * @scratchbuf: address of scratch buffer
  * @setup_buf: used while precessing STD USB requests
  * @ep0_trb_addr: dma address of @ep0_trb
  * @bounce_addr: dma address of @bounce
  * @ep0_usb_req: dummy req used while handling STD USB requests
+ * @scratch_addr: dma address of scratchbuf
  * @ep0_in_setup: one control transfer is completed and enter setup phase
  * @lock: for synchronizing
  * @mutex: for mode switching
@@ -1039,6 +1048,7 @@ struct dwc3_glue_ops {
  * @current_otg_role: current role of operation while using the OTG block
  * @desired_otg_role: desired role of operation while using the OTG block
  * @otg_restart_host: flag that OTG controller needs to restart host
+ * @nr_scratch: number of scratch buffers
  * @u1u2: only used on revisions <1.83a for workaround
  * @maximum_speed: maximum speed requested (mainly for testing purposes)
  * @max_ssp_rate: SuperSpeed Plus maximum signaling rate and lane count
@@ -1056,6 +1066,7 @@ struct dwc3_glue_ops {
  * @hsphy_mode: UTMI phy mode, one of following:
  *		- USBPHY_INTERFACE_MODE_UTMI
  *		- USBPHY_INTERFACE_MODE_UTMIW
+ * @dwc3_pmu: acquire and control power management unit(PMU) regulator
  * @role_sw: usb_role_switch handle
  * @role_switch_default_mode: default operation mode of controller while
  *			usb role is USB_ROLE_NONE.
@@ -1102,6 +1113,7 @@ struct dwc3_glue_ops {
  * @delayed_status: true when gadget driver asks for delayed status
  * @ep0_bounced: true when we used bounce buffer
  * @ep0_expect_in: true when we expect a DATA IN transfer
+ * @has_hibernation: true when dwc3 was configured with Hibernation
  * @sysdev_is_parent: true when dwc3 device has a parent driver
  * @has_lpm_erratum: true when core was configured with LPM Erratum. Note that
  *			there's now way for software to detect this in runtime.
@@ -1159,6 +1171,7 @@ struct dwc3_glue_ops {
  *	1	- -3.5dB de-emphasis
  *	2	- No de-emphasis
  *	3	- Reserved
+ * @is_hibernated: true when dwc3 is hibernated; abort processing events
  * @dis_metastability_quirk: set to disable metastability quirk.
  * @dis_split_quirk: set to disable split boundary.
  * @sys_wakeup: set if the device may do system wakeup.
@@ -1177,6 +1190,12 @@ struct dwc3_glue_ops {
  * @gsbuscfg0_reqinfo: store GSBUSCFG0.DATRDREQINFO, DESRDREQINFO,
  *		       DATWRREQINFO, and DESWRREQINFO value passed from
  *		       glue driver.
+ * @is_d3: set if the controller is in d3 state
+ * @saved_regs: registers to be saved/restored during hibernation/wakeup events
+ * @irq_wakeup: wakeup IRQ number, triggered when host asks to wakeup from
+ *              hibernation
+ * @force_hiber_wake: flag set when the gadget driver is forcefully triggering
+ *		a hibernation wakeup event
  * @wakeup_pending_funcs: Indicates whether any interface has requested for
  *			 function wakeup in bitmap format where bit position
  *			 represents interface_id.
@@ -1185,9 +1204,11 @@ struct dwc3 {
 	struct work_struct	drd_work;
 	struct dwc3_trb		*ep0_trb;
 	void			*bounce;
+	void			*scratchbuf;
 	u8			*setup_buf;
 	dma_addr_t		ep0_trb_addr;
 	dma_addr_t		bounce_addr;
+	dma_addr_t		scratch_addr;
 	struct dwc3_request	ep0_usb_req;
 	struct completion	ep0_in_setup;
 
@@ -1242,6 +1263,7 @@ struct dwc3 {
 	struct extcon_dev	*edev;
 	struct notifier_block	edev_nb;
 	enum usb_phy_interface	hsphy_mode;
+	struct regulator	*dwc3_pmu;
 	struct usb_role_switch	*role_sw;
 	enum usb_dr_mode	role_switch_default_mode;
 
@@ -1254,6 +1276,7 @@ struct dwc3 {
 	u32			current_otg_role;
 	u32			desired_otg_role;
 	bool			otg_restart_host;
+	u32			nr_scratch;
 	u32			u1u2;
 	u32			maximum_speed;
 	u32			gadget_max_speed;
@@ -1357,6 +1380,7 @@ struct dwc3 {
 	unsigned		delayed_status:1;
 	unsigned		ep0_bounced:1;
 	unsigned		ep0_expect_in:1;
+	unsigned		has_hibernation:1;
 	unsigned		sysdev_is_parent:1;
 	unsigned		has_lpm_erratum:1;
 	unsigned		is_utmi_l1_suspend:1;
@@ -1396,6 +1420,7 @@ struct dwc3 {
 
 	unsigned		tx_de_emphasis_quirk:1;
 	unsigned		tx_de_emphasis:2;
+	unsigned		is_hibernated:1;
 
 	unsigned		dis_metastability_quirk:1;
 
@@ -1413,6 +1438,10 @@ struct dwc3 {
 	int			num_ep_resized;
 	struct dentry		*debug_root;
 	u32			gsbuscfg0_reqinfo;
+	bool			is_d3;
+	u32			*saved_regs;
+	u32			irq_wakeup;
+	bool			force_hiber_wake;
 	u32			wakeup_pending_funcs;
 };
 
@@ -1582,6 +1611,8 @@ void dwc3_set_prtcap(struct dwc3 *dwc, u32 mode, bool ignore_susphy);
 void dwc3_set_mode(struct dwc3 *dwc, u32 mode);
 u32 dwc3_core_fifo_space(struct dwc3_ep *dep, u8 type);
 
+typedef void (*dwc3_wakeup_t)(struct device *dev, bool wakeup);
+
 #define DWC3_IP_IS(_ip)							\
 	(dwc->ip == _ip##_IP)
 
@@ -1643,12 +1674,17 @@ static inline void dwc3_pre_run_stop(struct dwc3 *dwc, bool is_on)
 #if IS_ENABLED(CONFIG_USB_DWC3_HOST) || IS_ENABLED(CONFIG_USB_DWC3_DUAL_ROLE)
 int dwc3_host_init(struct dwc3 *dwc);
 void dwc3_host_exit(struct dwc3 *dwc);
+void dwc3_host_wakeup_register(dwc3_wakeup_t func);
 #else
 static inline int dwc3_host_init(struct dwc3 *dwc)
 { return 0; }
 static inline void dwc3_host_exit(struct dwc3 *dwc)
 { }
+static inline void dwc3_host_wakeup_register(dwc3_wakeup_t func)
+{ }
 #endif
+
+int dwc3_core_init(struct dwc3 *dwc);
 
 #if IS_ENABLED(CONFIG_USB_DWC3_GADGET) || IS_ENABLED(CONFIG_USB_DWC3_DUAL_ROLE)
 int dwc3_gadget_init(struct dwc3 *dwc);
@@ -1662,6 +1698,8 @@ int dwc3_send_gadget_generic_command(struct dwc3 *dwc, unsigned int cmd,
 		u32 param);
 void dwc3_gadget_clear_tx_fifos(struct dwc3 *dwc);
 void dwc3_remove_requests(struct dwc3 *dwc, struct dwc3_ep *dep, int status);
+void dwc3_gadget_enter_hibernation(struct dwc3 *dwc);
+void dwc3_gadget_exit_hibernation(void *_dwc);
 #else
 static inline int dwc3_gadget_init(struct dwc3 *dwc)
 { return 0; }
@@ -1682,6 +1720,10 @@ static inline int dwc3_send_gadget_generic_command(struct dwc3 *dwc,
 		int cmd, u32 param)
 { return 0; }
 static inline void dwc3_gadget_clear_tx_fifos(struct dwc3 *dwc)
+{ }
+static inline void dwc3_gadget_enter_hibernation(struct dwc3 *dwc)
+{ }
+static inline void dwc3_gadget_exit_hibernation(void *_dwc)
 { }
 #endif
 
@@ -1733,5 +1775,7 @@ static inline int dwc3_ulpi_init(struct dwc3 *dwc)
 static inline void dwc3_ulpi_exit(struct dwc3 *dwc)
 { }
 #endif
+void dwc3_free_event_buffers(struct dwc3 *dwc);
+int dwc3_event_buffers_setup(struct dwc3 *dwc);
 
 #endif /* __DRIVERS_USB_DWC3_CORE_H */
